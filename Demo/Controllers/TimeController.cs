48d5033d36e1f216754a21df54cd95ed416ee32f
Fa <francois.alyanakian@frtee.fr>
2026-03-22 23:22:23 +0100

    Ajout menu WebApp

--- Demo/Controllers/TimeController.cs
+++ Demo/Controllers/TimeController.cs
@@ -5,7 +5,7 @@ namespace Demo.Controllers;
 
 public class TimeController : Controller
 {
-    public IActionResult Index(bool useSlider = true)
+    public IActionResult Index(bool useSlider = false)
     {
         var start = DateTime.Now.AddYears(-5);
         var end = DateTime.Now;
